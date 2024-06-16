97de87bbc9825bd13317ce59a3d1f9c9807152cf
Sumeet Badgujar <41300544+Haikoitoh@users.noreply.github.com>
2024-06-15 20:26:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 import plotly.express as px
+import datetime
 
 st.set_page_config(
     page_title="Streaming Service Analytics",
